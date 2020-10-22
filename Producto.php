5f07cae8e121087272a8fb415d69a72324857631
egilmijail <egilmijail@hotmail.com>
2020-10-22 18:04:17 -0500

    Entidad Producto

---
+++ Producto.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Tiendita;
+
+
+class Producto
+{
+    public $IdProducto=0;
+    public $Clave="";
+    public $Nombre="";
+    public $Descripcion="";
+    public $Costo=0.0;
+
+}
