4c2362847184d6b7d2dbc1ad0265a7bdedab8351
TiemposHQ <tiempos.shop@gmail.com>
2020-10-23 14:17:10 -0500

    Direcciones.php

---
+++ Direcciones.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Tiendita;
+
+
+class Direcciones
+{
+
+    public $IdDireccion = 0 ;
+    public $Pais = "" ;
+    public $CodigoPostal = "" ;
+    public $Ciudad = "" ;
+    public $Calle = "" ;
+    public $Colonia = "" ;
+
+
+}
