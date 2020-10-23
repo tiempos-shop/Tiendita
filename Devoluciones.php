e151d7eb04b516445935fa3575c67808763100a3
TiemposHQ <tiempos.shop@gmail.com>
2020-10-23 13:45:35 -0500

    formato

---
+++ Devoluciones.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Tiendita;
+
+
+class Devoluciones
+{
+
+    public $IdDevolucion = 0 ;
+    public $IdPedido = 0 ;
+    public $IdMotivoDevolucion = "" ;
+
+}
--- Producto.php
+++ Producto.php
@@ -6,10 +6,10 @@ namespace Tiendita;
 
 class Producto
 {
-    public $IdProducto=0;
-    public $Clave="";
-    public $Nombre="";
-    public $Descripcion="";
-    public $Costo=0.0;
+    public $IdProducto = 0 ;
+    public $Clave = "" ;
+    public $Nombre = "" ;
+    public $Descripcion = "" ;
+    public $Costo = 0.0 ;
 
 }
