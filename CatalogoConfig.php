c6ab9e401d24aa3a7da545a81eff2bb09da6ecf8
TiemposHQ <tiempos.shop@gmail.com>
2020-10-23 13:42:52 -0500

    Pedidos

---
+++ CatalogoConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Tiendita;
+
+
+class CatalogoConfig
+{
+
+    public $IdConfig = 0 ;
+    public $Concepto = "" ;
+    public $Valor = 0.0 ;
+    public $Id = 0 ; // NUM o STR ?
+
+}
---
+++ Pedidos.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Tiendita;
+
+
+class Pedidos
+{
+
+    public $IdPedido = 0 ;
+    public $EstatusPedido = "" ; // bool?
+    public $IdEnvio = 0 ;
+    public $FechaPedido = "" ; // día mes año
+    public $IdCliente = 0 ;
+
+}
