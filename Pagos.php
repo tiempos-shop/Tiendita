bb8d39293f390681239b3c7554278520bea67462
TiemposHQ <tiempos.shop@gmail.com>
2020-10-23 14:20:28 -0500

    Pagos.php

---
+++ Pagos.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Tiendita;
+
+
+class Pagos
+{
+
+    public $IdPago = 0 ;
+    public $Descripcion = "" ; // TIPO DE VARIABLE?
+    public $Compania = "" ;
+    public $EstatusPago = false ; //Bool o STR?
+
+}
