d6c532fbd647b93ce2e17f4de928ebbb92b9bf5a
José Salinas <jose.salinas@softquimia.com>
2021-06-29 14:49:28 -0500

    Menu white
    Archivo: VistasHtml.php.php

--- View/Componentes/Administracion/VistasHtml.php
+++ View/Componentes/Administracion/VistasHtml.php
@@ -71,9 +71,9 @@ class VistasHtml
             $idioma[ $idiomaActual ]["FILTER"][$ordenamientoActual] = $idioma[ $idiomaActual ]["FILTER"][$ordenamientoActual].$caracterFocus;
         }
 
-        $nav  = "<nav id='menu-movil-dorado' class='navbar navbar-inverse navbar-static-top  d-sm-block d-md-none d-block d-block ".$clase."' style='position: fixed; width: 100%; top:0; z-index: 2;left: 0;' role='navigation'>
+        $nav  = "<nav id='menu-movil-dorado' class='navbar navbar-inverse navbar-static-top position-fixed d-sm-block d-md-none d-block d-block bg-transparent ".$clase."' style='width: 100%; top:0; z-index: 2;left: 0;' role='navigation'>
                         <div class=''>
-                    <div class='navbar-header d-flex justify-content-between align-items-center ml-2 mr-2'>
+                    <div class='navbar-header d-flex justify-content-between align-items-center ml-2 mr-2' style='margin-top: 0;'>
                     <button type='button' class='navbar-toggle collapsed ' data-toggle='collapse' id='botonMenuMovil'
                         onClick='".$funcionMenuCLick."'
                         ;
@@ -259,6 +259,59 @@ class VistasHtml
         return $html;
     }
 
+public function Carousel(){
+
+    $caro='<html> <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-6">
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">';
+
+      foreach ($images as $image){
+          $count++;
+          $id="id".$count;
+    //$collage.="<img id='$id' class='img-fluid' src='$image' data-big='$image' data-overlay=''><br/>";// data-overlay="fondo.png"
+$caro.=ImagenCarrusel($id,$image);
+
+  $caro.='</div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    </div>
+  </div>
+</div>
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+</html>';
+   return $caro;
+}
 
 
+    //$innerScript.=$fc->ScriptAmpliarFoto($id);
+}
+function ImagenCarrusel($id,$rutaImagen)
+{
+    return '<div class="item">
+        <img id="' . $id . '" src="' . $rutaImagen . '">
+      </div>';
+}
 }
