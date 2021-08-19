92bab88b9e9d5c45514d40644cdf41d086f8e260
José <jose.salinas@softquimia.com>
2021-08-18 19:36:57 -0500

    detalles arreglados

--- view.php
+++ view.php
@@ -364,7 +364,7 @@ $h= $html->Html5(
                             "</div><hr style='margin: 0 0 0 0'/>",
                             $fc->SizeButton($botonTalla,$opcionesTallas),
                             $flowButton,
-                            //$fc->MenuPrivacyReturnView(true,true)
+                            $fc->MenuPrivacyReturnView(true,true)
 
                         ],"component")
 
